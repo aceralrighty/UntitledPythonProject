d8dd0676ebbf7942a4a0b5df5db7db599c443f51
Adam <adamziv156@gmail.com>
2025-08-11 12:04:30 -0500

    Feels too easy with python

--- TF/Custom.py
+++ TF/Custom.py
@@ -1,15 +1,26 @@
-import keras_nlp
+import keras_hub
 import tensorflow_datasets as tfds
 
-imdb_train, imdb_test = tfds.load(
-  "imdb_reviews",
-  split=["train", "test"],
-  as_supervised=True,
-  batch_size=16,
+classifier = keras_hub.models.TextClassifier.from_preset(
+    "bert_base_en_uncased",
+    activation="softmax",
+    num_classes=2,
 )
-# Load a BERT model.
-classifier = keras_nlp.models.BertClassifier.from_preset("bert_base_en_uncased")
-# Fine-tune on IMDb movie reviews.
-classifier.fit(imdb_train, validation_data=imdb_test)
-# Predict two new examples.
-classifier.predict(["What an amazing movie!", "A total waste of my time."])
+
+imdb_train, imdb_test = tfds.load(
+    "imdb_reviews",
+    split=["train[:2000]", "test[:400]"],
+    as_supervised=True,
+    batch_size=32,
+)
+
+# Add training parameters to control time and prevent overfitting
+classifier.fit(
+    imdb_train.take(100),
+    validation_data=imdb_test.take(20),
+    epochs=3,  # BERT usually needs only 2-4 epochs
+    verbose=1
+)
+
+preds = classifier.predict(["What an amazing movie!", "A total waste of time."])
+print(preds)
